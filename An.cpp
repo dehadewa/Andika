2fde357f7b463b8135e87baa09d643e67ecba228
WINDOWS 11 <WINDOWS 11@DESKTOP-6GDLIJ0>
2024-10-31 10:17:56 +0700

    Add project files.

---
+++ An.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void hitunganEK(double massa, double kecepatan) {
+	double EK = massa + (kecepatan + kecepatan);
+	cout << "Energi Kinetiknya Adalah: " << EK << " Joule "; endl;
+}
+
+
+int main() {
+
+	double massa = 0, kecepatan = 0, Ek = 0;
+	count << "Menghitung Energi Kinetik: \n";
+	cout << "Masukan massa: ";
+	cin >> massa;
+	cout << "Masukan Kecepatan: ";
+	cin >> Kecepatan;
+
+	HitungEk(Massa, Kecepatan);
+
+	return 0;
+}
